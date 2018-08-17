99c44b1865483a2eb63408dc6542442624ab41b6
Colin Kiama <colinkiama@hotmail.co.uk>
2018-08-17 22:20:41 +0100

    You can now write rich text!

--- Write-A-Thon/Shell.xaml.cs
+++ Write-A-Thon/Shell.xaml.cs
@@ -12,19 +12,21 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Write_A_Thon.View;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Write_A_Thon
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Shell : Page
     {
-        public MainPage()
+        public Shell()
         {
-            this.InitializeComponent();
+            this.InitializeComponent(); 
+            mainFrame.Navigate(typeof(WritingView));
         }
     }
 }
---
+++ Write-A-Thon/ViewModel/WritingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_A_Thon.ViewModel
+{
+    public class WritingViewModel
+    {
+    }
+}
