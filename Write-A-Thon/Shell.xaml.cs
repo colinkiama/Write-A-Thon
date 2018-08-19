5f94a70128aac59bf8995b6cc020949333e34bbc
Colin Kiama <colinkiama@hotmail.co.uk>
2018-08-19 20:54:39 +0100

    Menu and settings buttons now show/hide their menus with smooth animations :)

--- Write-A-Thon/Shell.xaml.cs
+++ Write-A-Thon/Shell.xaml.cs
@@ -11,9 +11,11 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 using Write_A_Thon.View;
-
+using Microsoft.Toolkit.Uwp.UI.Animations;
+using System.Threading.Tasks;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Write_A_Thon
@@ -21,23 +23,86 @@ namespace Write_A_Thon
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+
     public sealed partial class Shell : Page
     {
         public Shell()
         {
-            this.InitializeComponent(); 
+            this.InitializeComponent();
             mainFrame.Navigate(typeof(WritingView));
             InfoBarFrame.Navigate(typeof(InfoBarView));
         }
 
-        private void MenuButton_Click(object sender, RoutedEventArgs e)
-        {
 
+        private async void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton menuToggle)
+            {
+                if ((bool)menuToggle.IsChecked)
+                {
+                    await ShowMenuGrid();
+                }
+                else
+                {
+                    await HideMenuGrid();
+
+                }
+            }
         }
 
-        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        private async Task ShowMenuGrid()
         {
+            await MenuGrid.Offset(0).StartAsync();
+        }
 
+
+        private async Task HideMenuGrid()
+        {
+            float widthToMove = (float)MenuGrid.ActualWidth;
+            await MenuGrid.Offset(-widthToMove).StartAsync();
+        }
+
+        private async Task HideMenuGrid(float duration = 500)
+        {
+            float widthToMove = (float)MenuGrid.ActualWidth;
+            await MenuGrid.Offset(-widthToMove,0,duration).StartAsync();
+        }
+
+        private async void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton settingsToggle)
+            {
+                if ((bool)settingsToggle.IsChecked)
+                {
+                    await ShowSettingsFrame();
+                }
+                else
+                {
+                    await HideSettingsFrame();
+                }
+            }
+        }
+
+        private async Task ShowSettingsFrame()
+        {
+            await SettingsFrame.Offset(0).StartAsync();
+        }
+
+        private async Task HideSettingsFrame(float duration = 500)
+        {
+            float widthToMove = (float)SettingsFrame.ActualWidth;
+            await SettingsFrame.Offset(widthToMove,0,duration).StartAsync();
+        }
+
+        private async void MenuGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            await HideMenuGrid(0);
+        }
+
+        private async void SettingsFrame_Loaded(object sender, RoutedEventArgs e)
+        {
+            await HideSettingsFrame(0);
         }
     }
 }
