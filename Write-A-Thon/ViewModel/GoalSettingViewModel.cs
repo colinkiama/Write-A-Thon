ba796bcbc60170542e15fc97d4966310dbb892c8
Colin Kiama <colinkiama@hotmail.co.uk>
2018-08-24 03:44:26 +0100

    Finished design of first step of goalSettingView

--- Write-A-Thon/ViewModel/GoalSettingViewModel.cs
+++ Write-A-Thon/ViewModel/GoalSettingViewModel.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 namespace Write_A_Thon.ViewModel
 {
-    class GoalSettingViewModel: NotifyingViewModel
+    class GoalSettingViewModel : NotifyingViewModel
     {
         private uint _totalWordsWritten;
 
         public uint TotalWordsWritten
         {
             get { return _totalWordsWritten; }
-            set { _totalWordsWritten = value; }
+            set
+            {
+                _totalWordsWritten = value;
+                NotifyPropertyChanged();
+            }
         }
+
+        private DateTimeOffset _selectedDueDate;
+
+        public DateTimeOffset SelectedDueDate
+        {
+            get { return _selectedDueDate; }
+            set
+            {
+                _selectedDueDate = value;
+                NotifyPropertyChanged();
+            }
+        }
+
 
     }
 }
