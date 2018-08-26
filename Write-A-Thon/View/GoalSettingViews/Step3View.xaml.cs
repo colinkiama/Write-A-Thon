848a9268f78488e0ed15abffe5f421cae9fbdcd9
Colin Kiama <colinkiama@hotmail.co.uk>
2018-08-26 02:43:21 +0100

    All the step views are done well.

--- Write-A-Thon/View/GoalSettingViews/Step3View.xaml.cs
+++ Write-A-Thon/View/GoalSettingViews/Step3View.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Write_A_Thon.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -30,7 +31,7 @@ namespace Write_A_Thon.View.GoalSettingViews
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-
+            ViewModel.GoalBeingCreated = (Goal)e.Parameter;
         }
     }
 }
--- Write-A-Thon/ViewModel/Step3ViewModel.cs
+++ Write-A-Thon/ViewModel/Step3ViewModel.cs
@@ -5,12 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 using Write_A_Thon.Commands;
 using Write_A_Thon.Helpers;
+using Write_A_Thon.Model;
 
 namespace Write_A_Thon.ViewModel
 {
     class Step3ViewModel: NotifyingViewModel
     {
         public RelayCommand NavBackCommand{ get; set; }
+
+        private Goal _goalBeingCreated;
+
+        public Goal GoalBeingCreated
+        {
+            get { return _goalBeingCreated; }
+            set
+            {
+                _goalBeingCreated = value;
+                UpdateGoalSummary();
+            }
+        }
+
+        private void UpdateGoalSummary()
+        {
+            uint wordsToWritePerDay = CalculateNumOfWordsToWritePerDay();
+            GoalSummaryString = $"Based on your answers, you should write {wordsToWritePerDay} words per day.";
+        }
+
+        private uint CalculateNumOfWordsToWritePerDay()
+        {
+            uint numOfWordsPerDay = 0;
+            if (GoalBeingCreated.TotalWordsPerDay > 0)
+            {
+                var currentDate = DateTimeOffset.UtcNow;
+                TimeSpan timeBetweenNowAndDeadline = GoalBeingCreated.DueDate - currentDate;
+                double totalDaysTillDeadline = timeBetweenNowAndDeadline.TotalDays;
+                if (totalDaysTillDeadline > 0)
+                {
+                    uint roundedUpDaysTillDeadline = 1;
+                    if (totalDaysTillDeadline > 1)
+                    {
+                        roundedUpDaysTillDeadline = (uint)Math.Ceiling(totalDaysTillDeadline);
+                    }
+                    numOfWordsPerDay = GoalBeingCreated.TotalWordsPerDay/ roundedUpDaysTillDeadline;
+                }
+
+            }
+            return numOfWordsPerDay;
+        }
 
         private string _goalSummaryString;
 
